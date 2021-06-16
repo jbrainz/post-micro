382aa2637e526357a7010dc2401f97af92d2466f
jbrainz <developer@roberronero.com>
2021-06-16 10:16:25 +0100

    url updat to k8s ):

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.json())
 
 app.post("/events", (req, res) => {
   const event = req.body
-  axios.post("http://localhost:4000/events", event)
-  axios.post("http://localhost:4001/events", event)
-  axios.post("http://localhost:4002/events", event)
-  axios.post("http://localhost:4003/events", event)
+  axios.post("http://post-clusterip-srv:4000/events", event)
+  axios.post("http://comments-srv:4001/events", event)
+  axios.post("http://query-srv:4002/events", event)
+  axios.post("http://moderation-srv:4003/events", event)
 
   res.send({
     status: "OK",
